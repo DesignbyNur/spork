e38fa490663e0260ee4953a9d904083d8018499a
robroy <me@robert-e-roy.com>
2015-11-20 12:57:48 -0800

    new file:   Start.py stuff

---
+++ Start.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import os
+import sys
+import pdb
+
+def main():
+    '''What wll we do!'''
+#    setup()
+    pdb.set_trace()
+    print 'hello'
+ 
+
+# Here's our payoff idiom!
+if __name__ == '__main__':
+    main()
+
+
